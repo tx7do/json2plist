80b8256994db2dfbc681182f34b50fc5506138c5
tx7do <yanglinbo@gmail.com>
2024-10-16 10:08:53 +0800

    feat: convert dir.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # json2plist
+
+Convert Egret JSON to Property List (plist) format.
+
+convert one single file:
+
+```shell
+go run main.go -f input.json
+```
+
+convert all json files in a directory:
+
+```shell
+go run main.go -d input_dir
+```
--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
 	"strings"
 
 	"json2plist/pkg/ccc"
@@ -23,7 +26,39 @@ func handleSingleFile(filePath string) bool {
 	return true
 }
 
+func getCurrentDirFiles(path string) ([]string, error) {
+	infos, err := os.ReadDir(path)
+	if err != nil {
+		return []string{}, nil
+	}
+	var currentFiles []string
+	for _, info := range infos {
+		abs, err := filepath.Abs(info.Name())
+		if err != nil {
+			return []string{}, nil
+		}
+
+		if info.IsDir() {
+			continue
+		}
+
+		if filepath.Ext(abs) != ".json" {
+			continue
+		}
+
+		currentFiles = append(currentFiles, abs)
+	}
+
+	return currentFiles, nil
+}
+
 func handleFolder(dir string) bool {
+	files, _ := getCurrentDirFiles(dir)
+	fmt.Println("files: ", files)
+
+	for _, file := range files {
+		handleSingleFile(file)
+	}
 
 	return true
 }
